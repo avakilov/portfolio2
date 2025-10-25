4814af75190b096c7edfb2fb2224bf8e64b1d337
Aryslanbek Vakilov <119982220+avakilov@users.noreply.github.com>
2025-10-24 17:13:42 -0700

    Update global.js

--- global.js
+++ global.js
@@ -13,3 +13,17 @@ let currentLink = navLinks.find(
 if (currentLink) {
   currentLink.classList.add("current");
 }
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
